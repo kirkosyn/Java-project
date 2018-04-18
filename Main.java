937a7255a2c33bf631f9148fe51c0486f99dd743
kirkosyn <kokoska300@op.pl>
2018-04-18 20:25:51 +0200

    Class starting the app

---
+++ Main.java
@@ -0,0 +1,19 @@
+import Windows.*;
+
+public class Main {
+
+    public static void main(String args[])
+    {
+        StoreWindow win = new StoreWindow();
+        SureWindow win2 = new SureWindow();
+        WinWindow win3 = new WinWindow();
+        ChangeShipsWindow win4 = new ChangeShipsWindow();
+        LevelsWindow win5 = new LevelsWindow();
+        NickWindow win6 = new NickWindow();
+        MenuWindow win7 = new MenuWindow();
+        RankingWindow win8 = new RankingWindow();
+
+        GameWindow win9 = new GameWindow();
+
+    }
+}
